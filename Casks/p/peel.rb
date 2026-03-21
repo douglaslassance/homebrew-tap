8c6a81432d4368d5915daa1fa57e9e20eb107d6c
Douglas Lassance <hey@douglaslassance.me>
2026-03-20 17:54:13 -0700

    peel 1.1.0

--- Casks/p/peel.rb
+++ Casks/p/peel.rb
@@ -1,14 +1,14 @@
 cask "peel" do
-  version "1.0.0"
-  sha256 "611a8218f4ca92fab0301e43f796956ce2d6c6dba4358906a25584dd8f1f4e00"
+  version "1.1.0"
+  sha256 "a1a4e3d09118f99a399351506ad8292402fa11365eb6c4bf5b9d2870798c3c23"
 
-  url "https://storage.douglaslassance.me/peel/peel-#{version}.dmg"
+  url "https://api.douglaslassance.me/peel/download/#{version}"
   name "Peel"
   desc "Browse different"
   homepage "https://peel.douglaslassance.me/"
 
   livecheck do
-    url "https://storage.douglaslassance.me/peel/peel.json"
+    url "https://api.douglaslassance.me/peel"
     strategy :json do |json|
       json["latest"]
     end

4ded8e4e29b572edf96069a1172f8348fbce3ab2
Douglas Lassance <hey@douglaslassance.me>
2026-09-06 07:35:15 -0700

    playa 1.0.0 (#28)

--- Casks/p/playa.rb
+++ Casks/p/playa.rb
@@ -1,6 +1,6 @@
 cask "playa" do
-  version "0.4.0"
-  sha256 "01b056d9c26159b4957b50bc123ed00e644dfa2de2a3cc2557eda67ea2b7c9bc"
+  version "1.0.0"
+  sha256 "aa45483ca08fa409b130d3f43fafcfa4ed9e7de2122780554f30c26180e06be9"
 
   url "https://api.douglaslassance.me/playa/download/#{version}/aarch64-apple-darwin"
   name "Playa"

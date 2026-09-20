4d1ad178270c2226582ff352514c6041be1c1c7d
Douglas Lassance <hey@douglaslassance.me>
2026-09-19 19:50:58 -0700

    trotter 0.3.0 (#34)

--- Casks/t/trotter.rb
+++ Casks/t/trotter.rb
@@ -1,6 +1,6 @@
 cask "trotter" do
-  version "0.2.0"
-  sha256 "ae1c4db518f66bd6af25c45e937ea45a001d0df72c9571c56299df468bb73b20"
+  version "0.3.0"
+  sha256 "1a7924a1e490dc717a8b9f9e466902be62abada90f3ed4526c0121b8e6375d63"
 
   url "https://api.douglaslassance.me/v1/trotter/download/#{version}/aarch64-apple-darwin"
   name "Trotter"
@@ -19,7 +19,7 @@ cask "trotter" do
   app "Trotter.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.douglaslassance.trotter",
-    "~/Library/Containers/com.douglaslassance.trotter",
+    "~/Library/Application Scripts/me.douglaslassance.trotter",
+    "~/Library/Containers/me.douglaslassance.trotter",
   ]
 end

b5be64c20f4fe5c1744ad6a92b308b13194a0c73
Douglas Lassance <hey@douglaslassance.me>
2026-09-20 04:35:12 +0200

    Drop the deprecated verified parameter

--- Casks/g/gitoptic.rb
+++ Casks/g/gitoptic.rb
@@ -2,8 +2,7 @@ cask "gitoptic" do
   version "0.3.0"
   sha256 "06ee66d37f59b5050b3f6fb24dca901fe4d12f7e15dee514c3339c6ee8d867ef"
 
-  url "https://api.douglaslassance.me/v1/gitoptic/download/#{version}/aarch64-apple-darwin",
-      verified: "api.douglaslassance.me/v1/gitoptic/"
+  url "https://api.douglaslassance.me/v1/gitoptic/download/#{version}/aarch64-apple-darwin"
   name "Gitoptic"
   desc "Visual diffs for binary files in Git"
   homepage "https://douglaslassance.me/gitoptic"
